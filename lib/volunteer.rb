683041042a9c560f97ed1df368802d357d6cfb25
Lela Smith <smith.lelaa@gmail.com>
2020-01-15 19:39:47 -0800

    make change to spec to pass code

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -8,18 +8,18 @@ class Volunteer
   end
 
   def self.all
-      returned_volunteers = DB.exec("SELECT * FROM volunteers;")
-      volunteers = []
-      returned_volunteers.each() do |volunteer|
-        name = volunteer.fetch("name")
-        project_id = volunteer.fetch("project_id").to_i
-        id = volunteer.fetch("id").to_i
-        volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
-      end
-      volunteers
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
     end
+    volunteers
+  end
 
-    def save
+  def save
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
@@ -29,14 +29,14 @@ class Volunteer
   end
 
   def self.find(id)
-  volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
-  name = volunteer.fetch("name")
-  project_id = volunteer.fetch("project_id").to_i
-  id = volunteer.fetch("id").to_i
-  Volunteer.new({:name => name, :project_id => project_id, :id => id})
-end
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    name = volunteer.fetch("name")
+    project_id = volunteer.fetch("project_id").to_i
+    id = volunteer.fetch("id").to_i
+    Volunteer.new({:name => name, :project_id => project_id, :id => id})
+  end
 
-def update(name, project_id)
+  def update(name, project_id)
     @name = name
     @project_id = project_id
     DB.exec("UPDATE volunteers SET name = '#{@name}', project_id = #{@project_id} WHERE id = #{@id};")
@@ -46,7 +46,7 @@ def update(name, project_id)
     DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
   end
 
-def project
+  def project
     Project.find(@project_id)
   end
 
